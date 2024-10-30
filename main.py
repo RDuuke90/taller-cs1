b0ea011287c8e0e0c925a7b484ddae880cb44f4a
RDuuke90 <115588732+RDuuke90@users.noreply.github.com>
2024-10-29 21:06:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 1-> Crear un repositorio -> git init
 2-> Crear rama principal -> git branch -M main | git branch -m <nombre>
 3-> Lista ramas -> git branch
-
 4-> Crear rama -> git branch funcionalidad | git branch <nombre>
 5-> Ver los archivos modificados -> git status
 6-> Agregar los achivos para guardarlos -> git add . | git add ruta/archivo
 7-> Crear un punto de guardado -> git commit -m "mensaje"
 8-> Agregar un remoto -> git remote add origin https://github.com/RDuuke90/taller-cs1.git | git remote add <nombre> <url> 
-9- Subiendo cambios -> git push origin main -> git push <remote> <rama>
-agregando correo: git config user.email "jduqueg2@tdea.edu.co"
-agregando nombre: git config user.name "Juan Duque"
+9-> Subiendo cambios -> git push origin main -> git push <remote> <rama>
+10-> Para descargar el contenido del remoto -> git pull origin main -> git pull <remote> <rama>
 '''
