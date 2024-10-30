f2c607683201c9d8af0abf99ed206a1fd91c473d
Juan Duque <jduqueg2@tdea.edu.co>
2024-10-29 20:56:46 -0500

    Creando main con pasos

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+1-> Crear un repositorio -> git init
+2-> Crear rama principal -> git branch -M main | git branch -m <nombre>
+3-> Lista ramas -> git branch
+
+4-> Crear rama -> git branch funcionalidad | git branch <nombre>
+5-> Ver los archivos modificados -> git status
+6-> Agregar los achivos para guardarlos -> git add . | git add ruta/archivo
+'''
