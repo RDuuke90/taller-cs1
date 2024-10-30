f8d18f69f3fe2a07232685ffdc7f05082d07696e
Juan Duque <jduqueg2@tdea.edu.co>
2024-10-29 21:04:28 -0500

    Agregando el rsto de pasos

--- main.py
+++ main.py
@@ -6,4 +6,9 @@
 4-> Crear rama -> git branch funcionalidad | git branch <nombre>
 5-> Ver los archivos modificados -> git status
 6-> Agregar los achivos para guardarlos -> git add . | git add ruta/archivo
+7-> Crear un punto de guardado -> git commit -m "mensaje"
+8-> Agregar un remoto -> git remote add origin https://github.com/RDuuke90/taller-cs1.git | git remote add <nombre> <url> 
+9- Subiendo cambios -> git push origin main -> git push <remote> <rama>
+agregando correo: git config user.email "jduqueg2@tdea.edu.co"
+agregando nombre: git config user.name "Juan Duque"
 '''
