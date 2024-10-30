ede1207e4dc84d8b73c1a8c4b9273859cf0d8bab
Juan Duque <jduqueg2@tdea.edu.co>
2024-10-29 21:12:34 -0500

    corrigiendo errores

--- main.py
+++ main.py
@@ -5,4 +5,8 @@
 4-> Crear rama -> git branch funcionalidad | git branch <nombre>
 5-> Ver los archivos modificados -> git status
 6-> Agregar los achivos para guardarlos -> git add . | git add ruta/archivo
+7-> Crear un punto de guardado -> git commit -m "mensaje"
+8-> Agregar un remoto -> git remote add origin https://github.com/RDuuke90/taller-cs1.git | git remote add <nombre> <url> 
+9-> Subiendo cambios -> git push origin main -> git push <remote> <rama>
+10-> Para descargar el contenido del remoto -> git pull origin main -> git pull <remote> <rama>
 '''
